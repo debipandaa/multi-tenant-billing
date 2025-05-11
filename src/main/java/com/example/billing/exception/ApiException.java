e2ad461b08c5504655e9e8579b2c7251062ec6de
debii <debipanda@outlook.com>
2025-05-10 22:08:47 -0700

    Update database migration scripts

---
+++ src/main/java/com/example/billing/exception/ApiException.java
@@ -0,0 +1,27 @@
+package com.example.billing.exception;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String string) {
+        super(string);
+
+        // TODO Auto-generated constructor stub
+    }
+}
+// auto-generated minor edit 6151
+// auto-generated minor edit 17005
+// auto-generated minor edit 15426
+// auto-generated minor edit 17323
+// auto-generated minor edit 3760
+// auto-generated minor edit 12575
+// auto-generated minor edit 2970
+// auto-generated minor edit 32302
+// Auto-generated change #55
+// Auto-generated change #75
+// minor edit 23147
+// minor edit 23867
+// minor edit 17571
+// minor edit 20560
+// Auto-generated change #35
+// Auto-generated change #50
+// Auto-generated change #56
