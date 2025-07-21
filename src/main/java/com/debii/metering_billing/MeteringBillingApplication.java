8fa0777d4a73537cbd425f1124f5ae6f459bb469
debi <debipanda@outlook.com>
2025-07-21 13:33:59 -0700

    Update main application class MeteringBillingApplication

--- src/main/java/com/debii/metering_billing/MeteringBillingApplication.java
+++ src/main/java/com/debii/metering_billing/MeteringBillingApplication.java
@@ -2,8 +2,10 @@ package com.debii.metering_billing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MeteringBillingApplication {
 
 	public static void main(String[] args) {
